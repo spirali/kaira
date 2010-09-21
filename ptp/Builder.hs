fb764228a643726c5a81e1817b81498f50c415c8
Stanislav Bohm <stanislav.bohm@vsb.cz>
2010-09-21 22:33:55 +0200

    Fixed bug in limit of input packing edges

--- ptp/Builder.hs
+++ ptp/Builder.hs
@@ -150,7 +150,7 @@ checkEdges network decls binded processedEdges (edge:rest) level okEvent | isNor
 {- Variant for packing edges -}
 checkEdges network decls binded processedEdges (edge:rest) level okEvent =
 	IStatement [] [
-		IIf (ExprCall "<" [ ExprCall "List.size" [ placeExpr ], ExprCall "-" [ limitExpr, ExprInt (length edgesWithSamePlace)]])
+		IIf (ExprCall "<" [ ExprCall "List.size" [ placeExpr ], ExprCall "+" [ limitExpr, ExprInt (length edgesWithSamePlace)]])
 			(IReturn (ExprInt 0)) INoop,
 		(checkEdges network decls binded (edge:processedEdges) rest (level + 1) okEvent)
 	]
