e14056fcd0271c214fdf13cfbf2577791cfe5a8b
Stanislav Bohm <stanislav.bohm@vsb.cz>
2010-11-02 23:11:36 +0100

    build.sh improved

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 echo "Building C library ... "
 cd `dirname $0`/lib || abort
-make || abort
+make || exit 1
 cd ..
 echo "Building Project-To-Program compiler ... "
-cd `dirname $0`/ptp || abort
-ghc --make Main.hs -o ptp || abort
+cd `dirname $0`/ptp || exit 1
+ghc --make Main.hs -o ptp || exit 1
+
+type -P mpicc &>/dev/null || {
+echo "MPI not found. Kaira is compiled without MPI support."
+echo "If you want to use MPI, please install MPI and run build.sh again."; exit 0; 
+}
+
+cd ../lib
+make mpi
