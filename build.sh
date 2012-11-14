a530e8c1f321bbae82f9b2e6e0e337d3a4075751
Stanislav Bohm <stanislav.bohm@vsb.cz>
2012-11-14 16:14:20 +0100

    build.sh updated

--- build.sh
+++ build.sh
@@ -16,12 +16,20 @@ scons -Q || exit 1
 
 echo "Building CaVerif ... "
 cd $KAIRA_ROOT/libs/caverif || exit 1;
-scons -Q || exit 1
+scons -Q || {
+	echo "------------------------------------------------------------------"
+	echo "Building of CaVerif failed."
+    echo "CaVerif is not necessary for basic usage of Kaira,"
+	echo "but some analysis will be not available"
+	echo "------------------------------------------------------------------"
+}
 
 type -P mpicc &>/dev/null || {
+echo "------------------------------------------------------------------"
 echo "MPI not found. Kaira is compiled without MPI support."
 echo "MPI is not necessary to running Kaira."
 echo "If you want to use MPI, please install MPI and run build.sh again."; exit 0;
+echo "------------------------------------------------------------------"
 }
 
 echo "Building Cailie-MPI ..."
