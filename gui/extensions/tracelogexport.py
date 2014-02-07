713e14793b03f2fe8b77a2b748ef99778c20b1f7
Martin Surkovsky <martin.surkovsky@gmail.com>
2014-02-07 23:17:31 +0100

    RF: extract `ExportRunInstance` into separate file

--- gui/extensions/tracelogexport.py
+++ gui/extensions/tracelogexport.py
@@ -21,79 +21,9 @@
 
 import extensions
 import datatypes
-import settingswindow
 import utils
-from runinstance import RunInstance
-from gtk import RESPONSE_APPLY
-
-class ExportRunInstance(RunInstance):
-
-    def __init__(self, tracelog, transitions, place_functions, columns):
-        RunInstance.__init__(self, tracelog.project, tracelog.process_count, tracelog.threads_count)
-        self.transitions = transitions
-        self.place_functions = place_functions
-        self.column_event = "Event" in columns
-        self.column_time = "Time" in columns
-        self.column_id = "ID" in columns
-        self.column_process = "Process" in columns
-        self.column_tet = "TET" in columns
-        self.column_value = bool(place_functions)
-        self.rows = []
-        self.tet = None
-
-    def pre_event(self):
-        RunInstance.pre_event(self)
-        self.tet = None
-
-    def transition_finished(self, process_id, thread_id, time):
-        activity = self.activites[process_id * self.threads_count + thread_id]
-        self.tet = time - activity.time
-        if activity.transition in self.transitions:
-            self.add_row("T", time, activity.transition.id, process_id, "")
-        RunInstance.transition_finished(self, process_id, thread_id, time)
-
-    def add_token(self, place_id, token_pointer, token_value, send_time):
-        RunInstance.add_token(self, place_id, token_pointer, token_value, send_time)
-        for pf in self.place_functions:
-            if pf[0].id == place_id:
-                self.add_row("A",
-                             self.last_event_time,
-                             place_id,
-                             self.last_event_process,
-                             token_value[pf[1]])
-
-    def add_row(self, event, time, id, process, value):
-        row = []
-        if self.column_event:
-            row.append(event)
-        if self.column_time:
-            row.append(time)
-        if self.column_id:
-            row.append(id)
-        if self.column_process:
-            row.append(process)
-        if self.column_tet:
-            row.append(self.tet)
-        if self.column_value:
-            row.append(value)
-        self.rows.append(row)
-
-    def create_table(self):
-        header = []
-        if self.column_event:
-            header.append("Event")
-        if self.column_time:
-            header.append("Time")
-        if self.column_id:
-            header.append("ID")
-        if self.column_process:
-            header.append("Process")
-        if self.column_tet:
-            header.append("TET")
-        if self.column_value:
-            header.append("Value")
-        return (header, self.rows)
-
+import exportri
+from exportri import ExportRunInstance
 
 class TracelogExport(extensions.Operation):
 
@@ -103,40 +33,12 @@ class TracelogExport(extensions.Operation):
     parameters = [ extensions.Parameter("Tracelog", datatypes.t_tracelog) ]
 
     def run(self, app, tracelog):
-        assistant = settingswindow.BasicSettingAssistant(2,
-                                                         "Export settings",
-                                                         app.window)
-        assistant.set_size_request(600, 600)
-        def page_1(setting):
-            w = settingswindow.SettingWidget()
-            data = [ (t.get_name_or_id(), t, False)
-                     for t in tracelog.project.nets[0].transitions() ]
-            w.add_checkbuttons_list("transitions", "Transitions", data, [ "Transition", "Export fire?" ] )
-            data = [ ("{0}/{1}".format(p.get_name_or_id(), tracing.name), (p, i), False)
-                     for p in tracelog.project.nets[0].places() for i, tracing in enumerate(p.tracing) ]
-            w.add_checkbuttons_list("place_functions", "Place\nFunctions", data, [ "Place/Function", "Export add token?" ] )
-            return w
-        def page_2(setting):
-            w = settingswindow.SettingWidget()
-            data = [ ("Event", "Event", False),
-                     ("Time", "Time", True),
-                     ("ID", "ID", False),
-                     ("Process", "Process", False),
-                     ("Transition Execution Time", "TET", False),
-                   ]
-            w.add_checkbuttons("columns", "Columns", data)
-            return w
 
-        assistant.append_setting_widget("Events", page_1)
-        assistant.append_setting_widget("Columns", page_2)
-
-        if assistant.run() != RESPONSE_APPLY:
+        settings = exportri.run_assistant(app, tracelog);
+        if settings is None:
             return
 
-        ri = ExportRunInstance(tracelog,
-                               assistant.get_setting("transitions"),
-                               assistant.get_setting("place_functions"),
-                               assistant.get_setting("columns"))
+        ri = ExportRunInstance(tracelog, *settings)
         tracelog.execute_events(ri)
         return extensions.Source("Tracelog Table " + utils.get_timestamp_string(),
                                  datatypes.t_table,
