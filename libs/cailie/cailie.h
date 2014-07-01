f568abf70d6ec6537c2f4037c5e341866cf82b78
Stanislav Bohm <stanislav.bohm@vsb.cz>
2014-07-01 15:07:06 +0200

    ENH: ca::all_processes introduces and examples updated

--- libs/cailie/cailie.h
+++ libs/cailie/cailie.h
@@ -15,8 +15,6 @@
 
 #include "token.h"
 #include "place.h"
-#include "thread.h"
-#include "net.h"
 #include "parameters.h"
 #include "usertools.h"
 
@@ -28,45 +26,6 @@
 
 namespace ca {
 
-class Context {
-	public:
-		Context(ThreadBase *thread, NetBase *net) : thread(thread), net(net) {}
-
-		void quit() {
-			thread->quit_all();
-		}
-
-		int process_id() const {
-			return thread->get_process_id();
-		}
-
-		int process_count() const {
-			return thread->get_process_count();
-		}
-
-		void trace_value(const std::string &str) {
-			TraceLog *tracelog = thread->get_tracelog();
-			if (tracelog) {
-				tracelog->trace_value(str);
-			}
-		}
-		void trace(const int value) {
-			TraceLog *tracelog = thread->get_tracelog();
-			if (tracelog) {
-				tracelog->trace_value(value);
-			}
-		}
-		void trace(const double value) {
-			TraceLog *tracelog = thread->get_tracelog();
-			if (tracelog) {
-				tracelog->trace_value(value);
-			}
-		}
-
-	protected:
-		ThreadBase *thread;
-		NetBase *net;
-};
 
 /* Main functions */
 void init(
--- libs/cailie/usertools.h
+++ libs/cailie/usertools.h
@@ -2,11 +2,57 @@
 #ifndef CA_USERTOOLS_H
 #define CA_USERTOOLS_H
 
+#include "thread.h"
+#include "net.h"
+
 #include <vector>
 
 namespace ca {
 
+class Context {
+	public:
+		Context(ThreadBase *thread, NetBase *net) : thread(thread), net(net) {}
+
+		void quit() {
+			thread->quit_all();
+		}
+
+		int process_id() const {
+			return thread->get_process_id();
+		}
+
+		int process_count() const {
+			return thread->get_process_count();
+		}
+
+		void trace_value(const std::string &str) {
+			TraceLog *tracelog = thread->get_tracelog();
+			if (tracelog) {
+				tracelog->trace_value(str);
+			}
+		}
+		void trace(const int value) {
+			TraceLog *tracelog = thread->get_tracelog();
+			if (tracelog) {
+				tracelog->trace_value(value);
+			}
+		}
+		void trace(const double value) {
+			TraceLog *tracelog = thread->get_tracelog();
+			if (tracelog) {
+				tracelog->trace_value(value);
+			}
+		}
+
+	protected:
+		ThreadBase *thread;
+		NetBase *net;
+};
+
 	std::vector<int> range(int from, int upto);
+	inline std::vector<int> all_processes(Context &ctx) {
+		return range(0, ctx.process_count());
+	}
 
 }
 
